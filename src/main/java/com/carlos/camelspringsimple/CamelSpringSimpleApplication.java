a66c1154cbc5d2c5d8d9f2dec137fc05d9aa275a
Carlos Villalvazo <zentrix.24@gmail.com>
2020-02-19 20:53:18 -0600

    Initial Files

---
+++ src/main/java/com/carlos/camelspringsimple/CamelSpringSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.camelspringsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelSpringSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelSpringSimpleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carlos/camelspringsimple/CamelSpringSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.camelspringsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelSpringSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
