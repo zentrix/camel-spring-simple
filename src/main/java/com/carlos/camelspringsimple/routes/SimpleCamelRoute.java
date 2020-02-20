c50ba240b19ff20a92e683a606ead0184916374a
Carlos Villalvazo <zentrix.24@gmail.com>
2020-02-19 20:57:05 -0600

    Creation of a camel CopyFile Route

---
+++ src/main/java/com/carlos/camelspringsimple/routes/SimpleCamelRoute.java
@@ -0,0 +1,22 @@
+package com.carlos.camelspringsimple.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleCamelRoute extends RouteBuilder {
+
+    @Autowired
+    Environment env;
+
+    @Override
+    public void configure() throws Exception {
+        from("timer:hello?period=10s")
+                .log("Timer invoke and the body ${body}")
+                .pollEnrich("file:data/input?delete=true&readLock=none")
+                .to("file:data/output");
+
+    }
+}
