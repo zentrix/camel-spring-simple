87e2fa20a747a64c32bd3b4da04780f9588b0ce5
Carlos Villalvazo <zentrix.24@gmail.com>
2020-03-04 21:10:07 -0600

    Add Logging configuration

--- src/main/java/com/carlos/camelspringsimple/routes/SimpleCamelRoute.java
+++ src/main/java/com/carlos/camelspringsimple/routes/SimpleCamelRoute.java
@@ -1,11 +1,13 @@
 package com.carlos.camelspringsimple.routes;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class SimpleCamelRoute extends RouteBuilder {
 
     @Autowired
@@ -13,6 +15,9 @@ public class SimpleCamelRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+
+
+        log.info("Starting the Camel Route");
 
         from("{{startRoute}}")
                 .log("Timer invoke and the body using dynamic values " + env.getProperty("message"))
@@ -23,6 +28,8 @@ public class SimpleCamelRoute extends RouteBuilder {
                         .log("mock env flow and the body is ${body}")
                 .end()
                 .to("{{toRoute1}}");
+
+        log.info("Ending the Camel Route");
 
     }
 }
